c6758b897b8d30a4af0896cd2122f7508c70f97c
Danny Wang (ThePeeps191) <74725787+ThePeeps191@users.noreply.github.com>
2023-03-13 13:59:19 -0600

    Create

---
+++ helloworld.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#define uwu int
+#define Uwu main
+#define uwU {
+#define uWu std
+#define UwU ::
+#define UWu cout
+#define uWU ;
+#define UWU "Hello World"
+#define oWo <<
+#define OWo endl
+#define oWO }
+#define twt (
+#define TWT )
+#define TwT string
+#define TWt cin
+#define tWT "Hello, "
+#define IwI >>
+#define AwA "Enter your name: "
+uwu Uwu twt TWT uwU uWu UwU UWu oWo UWU oWo uWu UwU OWo uWU oWO
